5708b0fa3f06eda93024866e8d9965c81866aa99
jqin <jonn.qin@morningstar.com>
2017-08-22 17:36:11 +0800

    Add project files.

---
+++ WebApi/ContactController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Common;
+
+namespace WebApi
+{
+    public class ContactController : ApiController
+    {
+        static List<Contact> contacts;
+        static ContactController()
+        {
+            contacts.Add(new Contact { Id = "001", Name = "zhang", PhoneNo = "0512-12345678", EmailAddress = "qinzhiqiang@qq.com", Address = "gd" });
+            contacts.Add(new Contact { Id = "002", Name = "wang", PhoneNo = "0512-12345678", EmailAddress = "qinzhiqiang@qq.com", Address = "gd" });
+            contacts.Add(new Contact { Id = "003", Name = "li", PhoneNo = "0512-12345678", EmailAddress = "qinzhiqiang@qq.com", Address = "gd" });
+            contacts.Add(new Contact { Id = "004", Name = "zhao", PhoneNo = "0512-12345678", EmailAddress = "qinzhiqiang@qq.com", Address = "gd" });
+            contacts.Add(new Contact { Id = "005", Name = "qin", PhoneNo = "0512-12345678", EmailAddress = "qinzhiqiang@qq.com", Address = "gd" });
+            contacts.Add(new Contact { Id = "006", Name = "aa", PhoneNo = "0512-12345678", EmailAddress = "qinzhiqiang@qq.com", Address = "gd" });
+        }
+
+        public IEnumerable<Contact> Get(string id = null)
+        {
+            return from contact in contacts
+                   where contact.Id == id || string.IsNullOrEmpty(id)
+                   select contact;
+        }
+
+        public void Post(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void Put(Contact contact)
+        {
+            contacts.Remove(contacts.First(c => c.Id == contact.Id));
+            contacts.Add(contact);
+        }
+
+        public void Remove(string id)
+        {
+            contacts.Remove(contacts.First(c => c.Id == id));
+        }
+    }
+}
